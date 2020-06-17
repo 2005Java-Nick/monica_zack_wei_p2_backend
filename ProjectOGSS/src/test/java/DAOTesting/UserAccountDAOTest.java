8bf754354ca2a15cbee7a89e8eb100e91e05e903
Wei Wu <60204220+weiwu1994@users.noreply.github.com>
2020-06-17 15:11:17 -0400

    test

--- ProjectOGSS/src/test/java/DAOTesting/UserAccountDAOTest.java
+++ ProjectOGSS/src/test/java/DAOTesting/UserAccountDAOTest.java
@@ -1,7 +1,5 @@
 package DAOTesting;
 
-import static org.junit.Assert.assertEquals;
-
 import javax.transaction.Transactional;
 
 import org.junit.Test;
@@ -15,10 +13,6 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import com.revature.config.AppConfig;
 import com.revature.dao.DriverDAO;
 import com.revature.dao.UserAccountDAO;
-import com.revature.model.AccountType;
-import com.revature.model.Driver;
-import com.revature.model.UserAccount;
-import com.revature.struct.Token;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
@@ -34,26 +28,6 @@ public class UserAccountDAOTest {
 	@Transactional
 	@Rollback(true)
 	public void test() {
-		UserAccount acc = new UserAccount();
-		acc.setUsername("UnitTestName1");
-		acc.setPassword("UnitTestPass2");
-		acc.setSessionToken("Atoken1");
-		acc.setPhoneNumber("test");
-		acc.setFirstname("OldName");
-		acc.setLastname("test");
-		acc.setPassword("test");
-		acc.setEmail("test");
-		acc.setAddress("test");
-		acc.setState("test");
-		acc.setCity("test");
-		acc.setZip("3fe");
-		AccountType atype = new AccountType(1, "customer");
-		acc.setAccountType(atype);
-		Driver driv = driverDao
-				.getDriverShiftStatus(new Token("QGpB60qbQug5C+6c45XI9zaPaaM/vqIUUBO7M8F48C39ROtEOniTzg=="));
-		UserAccount accTest = userAccDao.createUserAccount(acc);
-		accTest.setFirstname("NewName");
-		UserAccount updatedAcc = userAccDao.updateAccount(accTest);
-		assertEquals(updatedAcc.getFirstname(), "NewName");
+
 	}
 }
