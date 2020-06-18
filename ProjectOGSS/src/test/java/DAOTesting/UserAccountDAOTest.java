e758b6622d75e3e5f3b5d3461c90e14c4b275166
Wei Wu <60204220+weiwu1994@users.noreply.github.com>
2020-06-17 22:45:40 -0400

    test

--- ProjectOGSS/src/test/java/DAOTesting/UserAccountDAOTest.java
+++ ProjectOGSS/src/test/java/DAOTesting/UserAccountDAOTest.java
@@ -33,6 +33,6 @@ public class UserAccountDAOTest {
 	@Rollback(true)
 	public void test() {
 
-		System.out.println("TEST PRODUCT s  " + productsDao.getProducts());
+		System.out.println("TEST PRODUT s  " + productsDao.getProducts());
 	}
 }
